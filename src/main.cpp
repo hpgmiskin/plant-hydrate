f6a1394c8a1db13e547aac4e5ea608520e930a6d
Henry Miskin <hpgmiskin@gmail.com>
2019-06-23 20:38:57 +0100

    Control pump using water level sensor

--- src/main.cpp
+++ src/main.cpp
@@ -3,21 +3,25 @@
 
 #define PUMP_PIN 15
 #define WATER_SENSOR_PIN 14
+#define NEEDS_WATER_THRESHOLD 120
 
 void setup()
 {
-    // initSerial();
+    initSerial();
     pinMode(PUMP_PIN, OUTPUT);
     pinMode(WATER_SENSOR_PIN, INPUT);
 }
 
 void loop()
 {
-    // const int sensor_value = analogRead(WATER_SENSOR_PIN);
-    // print("sensor_value: ", sensor_value);
-    // delay(1000);
-    digitalWrite(PUMP_PIN, HIGH);   // turn the relay on (HIGH is the voltage level)
-    delay(2000);              // wait for a second
-    digitalWrite(PUMP_PIN, LOW);    // turn the relay off by making the voltage LOW
-    delay(2000);              // wait for a second
+    const int water_level = analogRead(WATER_SENSOR_PIN);
+    print("water_level: ", water_level);
+    if (water_level < NEEDS_WATER_THRESHOLD){
+        print("enable pump");
+        digitalWrite(PUMP_PIN, HIGH);
+    } else {
+        print("disable pump");
+        digitalWrite(PUMP_PIN, LOW);
+    }
+    delay(5000);
 }
