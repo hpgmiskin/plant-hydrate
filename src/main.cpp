d9ff24b20532ea0a40bff11a03a13540d8f7282b
Henry Miskin <hpgmiskin@gmail.com>
2019-06-23 18:29:35 +0100

    Adds pump control code

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,23 @@
 #include "Arduino.h"
 #include "printing_utilities.hpp"
 
+#define PUMP_PIN 15
 #define WATER_SENSOR_PIN 14
 
 void setup()
 {
-    initSerial();
+    // initSerial();
+    pinMode(PUMP_PIN, OUTPUT);
     pinMode(WATER_SENSOR_PIN, INPUT);
 }
 
 void loop()
 {
-    const int sensor_value = analogRead(WATER_SENSOR_PIN);
-    print("sensor_value: ", sensor_value);
-    delay(1000);
+    // const int sensor_value = analogRead(WATER_SENSOR_PIN);
+    // print("sensor_value: ", sensor_value);
+    // delay(1000);
+    digitalWrite(PUMP_PIN, HIGH);   // turn the relay on (HIGH is the voltage level)
+    delay(2000);              // wait for a second
+    digitalWrite(PUMP_PIN, LOW);    // turn the relay off by making the voltage LOW
+    delay(2000);              // wait for a second
 }
