0aa00a36d6919e1b0ecdcafbeaef6efbe11e8be6
Henry Miskin <hpgmiskin@gmail.com>
2021-03-28 16:56:39 +0100

    Remove pump control code

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,17 @@
 #include "Arduino.h"
 #include "printing_utilities.hpp"
 
-#define PUMP_PIN 15
-#define WATER_SENSOR_PIN 14
-#define NEEDS_WATER_THRESHOLD 120
+#define WATER_SENSOR_PIN 23
 
 void setup()
 {
     initSerial();
-    pinMode(PUMP_PIN, OUTPUT);
     pinMode(WATER_SENSOR_PIN, INPUT);
 }
 
 void loop()
 {
-    const int water_level = analogRead(WATER_SENSOR_PIN);
-    print("water_level: ", water_level);
-    if (water_level < NEEDS_WATER_THRESHOLD){
-        print("enable pump");
-        digitalWrite(PUMP_PIN, HIGH);
-    } else {
-        print("disable pump");
-        digitalWrite(PUMP_PIN, LOW);
-    }
-    delay(5000);
+    const int water_sensor = analogRead(WATER_SENSOR_PIN);
+    print("water_sensor: ", water_sensor);
+    delay(1000);
 }
