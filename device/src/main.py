e1e06e0d14d4643cac9cc3ae1b310388c59d824e
Henry Miskin <hpgmiskin@gmail.com>
2021-03-28 17:27:35 +0100

    Add basic reading from serial

--- device/src/main.py
+++ device/src/main.py
@@ -1,4 +1,23 @@
-import time
+import asyncio
+import serial_asyncio
 
-print("Hello world!")
-time.sleep(60)
+
+class Output(asyncio.Protocol):
+    def connection_made(self, transport):
+        self.transport = transport
+        print('port opened', transport)
+        transport.serial.rts = False
+        transport.write(b'hello world\n')
+
+    def data_received(self, data):
+        print('data received', repr(data))
+
+    def connection_lost(self, exc):
+        print('port closed')
+        asyncio.get_event_loop().stop()
+
+loop = asyncio.get_event_loop()
+coro = serial_asyncio.create_serial_connection(loop, Output, '/dev/cu.usbmodem63781901', baudrate=9600)
+loop.run_until_complete(coro)
+loop.run_forever()
+loop.close()
